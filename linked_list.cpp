b6f4fe97669c672c0ea9650b256f048dfb7ed6ba
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-07-25 18:00:08 -0500

    Well. we can traverse now!

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,4 +1,7 @@
 //g++ --std=c++20 linked_list.cpp
+//In a linked list the LIST is responsible for keeping track of itself. The list takes in a node and changes it.
+//The list also only needs a head (which is a pointer to the memory address of the first node) and a tempory pointer to do everything it needs.
+//To traverse the List, you start by pointing a tempory pointer to head. While the next part of head has a value, you make the temporary node that value, and repeat.
 #include <iostream>
 
 using namespace std;
@@ -6,50 +9,84 @@ using namespace std;
 class node{
     public:
     int value;
-     node* next;
+     node *next;
 
      node(int data_value){
        value = data_value;
-       next = NULL;
+        next = NULL;
      }
 };
 
 struct linked_list{
     public:
-    int length = 0;
-    node head = NULL;
-    node tail = NULL;
+    node* head;
 
-    linked_list(node start_node){
-        length += 1;
-        head = start_node;
-        tail = start_node;
+    linked_list(){
+        head = NULL;
     }
 
-    void add_node(node node_to_add){
-        length += 1;
-        tail.next = &node_to_add;
-        tail = node_to_add;
+    void add_node(int number_to_add){
+        node* node_to_add = new node(number_to_add);
+        if(head == NULL){
+            head = node_to_add;
+            return;
+        }
+        node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = node_to_add;
     }
-
+    void delete_node(int value_to_delete){
+        if(head == NULL){
+            cout << "this is EMPTY" << endl;
+            return;
+        }
+        node* current_temp;
+        current_temp = head;
+        node* past_temp;
+        while(current_temp != NULL){
+            past_temp = current_temp;
+            if(current_temp->value == value_to_delete){
+                past_temp = current_temp->next;
+                cout << "deleted node " << value_to_delete << endl;
+                return;
+            }
+            else{
+                current_temp = current_temp->next; //This is hopping to the next node.
+            }
+        }
+        cout << "Node not found" << endl;
+        
+    }
+    void traverse(){
+       node* pointer = head;
+       if(pointer == NULL){
+        cout << "This is EMPTY" << endl;
+       }
+        while(pointer != NULL){
+           cout << pointer->value << endl; 
+           pointer = pointer->next;
+        }
+        if(pointer == NULL){
+           cout << "end of linked list" << endl; 
+        }
+    }
 };
 
 int main(){
 
-node one(1);
-node two(2);
-node three(300);
-linked_list list(one);
-list.add_node(two);
-list.add_node(three);
-node current = list.head;
-node* next = &list.head.next;
 
-while(next != NULL){
-    cout << current.value << endl;
-    next = current.next;
-}
+linked_list list;
+list.add_node(1);
+list.add_node(2);
+list.add_node(300);
 
+list.traverse();
+list.delete_node(4);
+list.add_node(420);
+list.delete_node(2);
+list.traverse();
 
 return 0;
 }
